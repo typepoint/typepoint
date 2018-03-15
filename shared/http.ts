e0a86047a5b852ff651787f1b39a5fa55ce553b0
Ben <codeandcats@gmail.com>
2018-03-15 20:36:16 +1100

    Allow cleanseHttpMethod to accept string

--- shared/http.ts
+++ shared/http.ts
@@ -1,5 +1,7 @@
 import tuple from 'typed-tuple';
 
+// TODO: Allow for full list of standard methods as well as custom methods
+
 export const HttpMethods = tuple('GET', 'PUT', 'POST', 'PATCH', 'DELETE');
 
 export type HttpMethod = typeof HttpMethods[number];
@@ -10,10 +12,10 @@ export class UnsupportedHttpMethod extends Error {
   }
 }
 
-export function cleanseHttpMethod(method: HttpMethod): HttpMethod {
-  method = method.toUpperCase() as HttpMethod;
+export function cleanseHttpMethod(method: string): HttpMethod {
+  method = method.toUpperCase();
   if (!HttpMethods.some(m => m === method)) {
     throw new UnsupportedHttpMethod(method);
   }
-  return method;
+  return method as HttpMethod;
 }
