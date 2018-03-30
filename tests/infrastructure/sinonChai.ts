6a12d3689b3f7936ae2b75323535a4f3935c512a
Ben <codeandcats@gmail.com>
2018-03-30 14:57:25 +1100

    Add chai-as-promised test library

--- tests/infrastructure/sinonChai.ts
+++
@@ -1,4 +0,0 @@
-import * as sinonChai from 'sinon-chai';
-import * as chai from 'chai';
-
-chai.use(sinonChai);
---
+++ tests/infrastructure/testSetup.ts
@@ -0,0 +1,6 @@
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import * as chaiAsPromised from 'chai-as-promised';
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
