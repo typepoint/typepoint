12ea99adb526481e7373ff559009b203165bc5eb
Ben <codeandcats@gmail.com>
2019-10-24 13:13:53 +1100

    ci(core): copy changelog to dist on build/release

--- support/build.sh
+++ support/build.sh
@@ -26,4 +26,8 @@ if test -d "$DIST_PATH"; then
   cp "$ROOT_PATH/LICENSE" ./dist
 
   yarn ts-node "$ROOT_PATH/support/copyPackage.ts" $1
+
+  if test -f "$PACKAGE_PATH/CHANGELOG.md"; then
+    cp "$PACKAGE_PATH/CHANGELOG.md" "$DIST_PATH/CHANGELOG.md"
+  fi
 fi
