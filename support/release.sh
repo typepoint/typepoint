4650bb0b80b78524f30bfa85a459751dce07ab3b
Ben <codeandcats@gmail.com>
2019-10-27 20:27:39 +1100

    ci(core): delete tags created by standard-version

--- support/release.sh
+++ support/release.sh
@@ -15,6 +15,15 @@ git pull origin master
 # to update CHANGE_LOG, and package.json version
 yarn standard-version --path "$PACKAGE_PATH"
 
+# Remove git tags added by standard-version.
+# This is to solve a problem standard-version runs into
+# when trying to add a git tag for a version of a package,
+# which has already been added for another package in the
+# repo. Git tags are kind of meaningless in a mono-repo
+# of multiple packages.
+TAGS="$(git tag -l "v*.*.*")"
+git tag -d $TAGS
+
 # Copy the package.json to the dist folder again
 # now that standard-version has updated the version
 yarn ts-node "$ROOT_PATH/support/copyPackage.ts" $1
