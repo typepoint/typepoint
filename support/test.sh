2b4d98f2349b5603d5b8791b822a2ea07e713e5a
Ben <codeandcats@gmail.com>
2019-11-30 11:42:53 +1100

    test(core): add support for testing individual files

--- support/test.sh
+++ support/test.sh
@@ -1,6 +1,7 @@
 SCRIPT_PATH=$(dirname "$0")
 ROOT_PATH="$SCRIPT_PATH/.."
 PACKAGE_PATH="$SCRIPT_PATH/../packages/$1"
+FILE_TO_TEST="$2"
 
 cd "$PACKAGE_PATH"
 
@@ -13,4 +14,4 @@ else
   cp "$ROOT_PATH/jest.config.json" "./"
 fi
 
-yarn jest --config "$JEST_CONFIG_FILE_NAME"
+yarn jest --config "$JEST_CONFIG_FILE_NAME" "$FILE_TO_TEST"
