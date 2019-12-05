04d89a72d848a9f298f0ac0f4a21d93dd6865d9b
Ben <codeandcats@gmail.com>
2019-12-06 09:35:13 +1100

    test(core): add 100% test coverage

--- packages/joiful/src/index.test.ts
+++ packages/joiful/src/index.test.ts
@@ -1,7 +1,9 @@
+/* eslint-disable max-classes-per-file */
 import 'reflect-metadata';
 
 import * as jf from 'joiful';
 import { ValidateAndTransformFunction } from '@typepoint/server';
+import { arrayOf, Empty, Constructor } from '@typepoint/shared';
 import { getValidateAndTransformFunction } from '.';
 
 describe('index', () => {
@@ -12,6 +14,46 @@ describe('index', () => {
   });
 
   describe('getValidateAndTransformFunction', () => {
+    it('should allow passing a custom joi validator', () => {
+      class Todo {
+        @jf.number().integer().optional()
+        id?: number;
+
+        @jf.string().min(1).required()
+        title = '';
+
+        @jf.boolean().required()
+        isCompleted = false;
+      }
+
+      const todo = {
+        id: '1',
+        title: 'Do the dishes',
+        isCompleted: false,
+        extraProperty: 'foo',
+      };
+
+      let result = validateAndTransform(todo, Todo);
+      expect(result).toHaveProperty(
+        ['validationError', 'message'],
+        '"extraProperty" is not allowed',
+      );
+
+      const validator = new jf.Validator({
+        allowUnknown: true,
+      });
+
+      validateAndTransform = getValidateAndTransformFunction({
+        validator,
+      });
+
+      result = validateAndTransform(todo, Todo);
+      expect(result).not.toHaveProperty(
+        ['validationError', 'message'],
+        '"extraProperty" is not allowed',
+      );
+    });
+
     describe('when validating input against a class with joiful decorators', () => {
       class Todo {
         @jf.number().integer().optional()
@@ -89,6 +131,77 @@ describe('index', () => {
           isCompleted: 'true',
         },
       });
+    });
+  });
+
+  describe('when validating input against an array of class', () => {
+    class Todo {
+      @jf.number().integer().optional()
+      id?: number;
+
+      @jf.string().min(1).required()
+      title = '';
+
+      @jf.boolean().required()
+      isCompleted = false;
+    }
+
+    it('should return transformed result when input is valid', () => {
+      const input = [{
+        id: '1',
+        title: 'Walk the cats',
+        isCompleted: 'true',
+      }];
+      const result = validateAndTransform(input, arrayOf(Todo));
+      expect(result).toEqual({
+        value: [{
+          id: 1,
+          title: 'Walk the cats',
+          isCompleted: true,
+        }],
+      });
+    });
+
+    it('should return validation error when input is invalid', () => {
+      const input: any = [{
+        title: '',
+      }];
+      expect(validateAndTransform(input, arrayOf(Todo))).toHaveProperty(
+        ['validationError', 'message'],
+        '"value" at position 0 fails because [child "title" fails because ["title" is not allowed to be empty]]',
+      );
+    });
+
+    it('should return as is when ArrayOf descriptor does not include an element class', () => {
+      const input = [{
+        title: 'Walk the cats',
+        foo: 'bar',
+      }];
+      expect(validateAndTransform(input, arrayOf(null as any as Constructor<any>)))
+        .toEqual({
+          value: [{
+            title: 'Walk the cats',
+            foo: 'bar',
+          }],
+        });
+    });
+  });
+
+  describe('when validating input that is expected to be Empty', () => {
+    it('should return result as is, if it is Empty', () => {
+      expect(validateAndTransform(null, Empty)).toEqual({ value: null });
+      expect(validateAndTransform(undefined, Empty)).toEqual({ value: undefined });
+      expect(validateAndTransform({}, Empty)).toEqual({ value: {} });
+    });
+
+    it('should return validation error when input is not Empty', () => {
+      const input: any = [{
+        title: 'Foo',
+      }];
+      expect(validateAndTransform(input, Empty)).toHaveProperty(
+        ['validationError', 'message'],
+        'Value is not empty',
+      );
     });
   });
 });
