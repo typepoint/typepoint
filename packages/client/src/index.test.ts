b6e8e4aa3546ae97b622569a445932495c5ae7fb
Ben <codeandcats@gmail.com>
2020-03-08 14:48:56 +1100

    test: add tests to ensure correct fetch function shape & optionality

--- packages/client/src/index.test.ts
+++ packages/client/src/index.test.ts
@@ -1,8 +1,24 @@
 import axios, { AxiosInstance } from 'axios';
-import { defineEndpoint, Empty, EndpointDefinition } from '@typepoint/shared';
+import {
+  defineEndpoint,
+  Empty,
+  EndpointDefinition,
+} from '@typepoint/shared';
 import * as fixtures from '@typepoint/fixtures';
 import { partialOf } from 'jest-helpers';
-import { TypePointClient, TypePointClientResponseError, TypePointClientResponse } from '.';
+import {
+  assert, Extends, Equal, Not, UnionIncludesExact,
+} from 'type-assertions';
+import {
+  FetchFunctionWithRequiredOptions,
+  FetchFunctionWithOptionalOptions,
+  FetchFunction,
+  FetchParamsOptions,
+  FetchBodyOptions,
+  TypePointClient,
+  TypePointClientResponseError,
+  TypePointClientResponse,
+} from './index';
 
 
 describe('TypePointClient', () => {
@@ -132,6 +148,59 @@ describe('TypePointClient', () => {
       method: 'POST',
       url: '/api/todos',
     });
+  });
+
+  it('allows request options to be optional when params and body not needed', () => {
+    assert<Equal<FetchFunction, FetchFunctionWithOptionalOptions & FetchFunctionWithRequiredOptions>>();
+
+    type FirstParam = Parameters<FetchFunctionWithOptionalOptions>[0];
+    type SecondParam = Parameters<FetchFunctionWithOptionalOptions>[1];
+    assert<Extends<FirstParam, EndpointDefinition<any, any, any>>>();
+
+    // Ensure 2nd param can be undefined
+    assert<UnionIncludesExact<SecondParam, undefined>>();
+
+    type EmptyParamsOptions = FetchParamsOptions<Empty>;
+    assert<Equal<EmptyParamsOptions, {}>>();
+
+    type EmptyBodyOptions = FetchBodyOptions<Empty>;
+    assert<Equal<EmptyBodyOptions, {}>>();
+  });
+
+  it('requires request options to be optional when params or body are required', () => {
+    type FirstParam = Parameters<FetchFunctionWithRequiredOptions>[0];
+    type SecondParam = Parameters<FetchFunctionWithRequiredOptions>[1];
+    assert<Extends<FirstParam, EndpointDefinition<any, any, any>>>();
+
+    // Ensure 2nd param cannot be undefined
+    assert<Not<UnionIncludesExact<SecondParam, undefined>>>();
+
+    assert<Extends<SecondParam, {} | { params: any } | { body: any } | { params: any; body: any }>>();
+
+    type UpdateTodoParams = {
+      id: string;
+    }
+
+    type UpdateTodoParamsOptions = FetchParamsOptions<UpdateTodoParams>;
+
+    assert<Not<Equal<UpdateTodoParamsOptions, {}>>>();
+    assert<Not<Equal<UpdateTodoParamsOptions, { params: { } }>>>();
+    assert<Not<Equal<UpdateTodoParamsOptions, { params: { id?: string } }>>>();
+
+    assert<Equal<UpdateTodoParamsOptions, { params: { id: string } }>>();
+
+    type UpdateTodoBody = {
+      title: string;
+      isCompleted?: boolean;
+    }
+
+    type UpdateTodoBodyOptions = FetchBodyOptions<UpdateTodoBody>;
+    assert<Not<Equal<UpdateTodoBodyOptions, {}>>>();
+    assert<Not<Equal<UpdateTodoBodyOptions, { body: { } }>>>();
+    assert<Not<Equal<UpdateTodoBodyOptions, { body: { title?: string } }>>>();
+
+    assert<Equal<UpdateTodoBodyOptions, { body: { title: string } }>>();
+    assert<Equal<UpdateTodoBodyOptions, { body: { title: string; isCompleted?: boolean } }>>();
   });
 });
 
